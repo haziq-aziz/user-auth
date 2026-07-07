5ae7442cb8ed21054b0ba9fdebf2c2d5f9e448f0
haziq-aziz <hxziqaziz@gmail.com>
2026-07-08 00:44:00 +0800

    fix: redirect user to login on index.tsx

--- src/app/(auth)/login.tsx
+++ src/app/(auth)/login.tsx
@@ -1,10 +1,11 @@
 import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function LoginScreen() {
 
     return (
-      <>
-        <Text>Login Screen</Text>
-      </>
+      <SafeAreaView className="flex-1">
+        <Text className="text-2xl font-bold text-center">Login Screen</Text>
+      </SafeAreaView>
     );
 }
